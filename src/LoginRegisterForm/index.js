1dba62a9816683c5f28b2c0738bee28f05219f71
Omar Amara <O_amara@hotmail.com>
2020-02-18 12:38:17 -0600

    installed/ imported semantic-ui, loginregister component displays

---
+++ src/LoginRegisterForm/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+export default class LoginRegisterForm extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			login: null
+		}
+	}
+
+	render() {
+		return(
+			<React.Fragment>
+				<h1>LoginRegisterForm</h1>
+			</React.Fragment>
+		)
+	}
+}
